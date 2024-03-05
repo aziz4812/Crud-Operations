3aa7414b8f317abbaa9d830de407109919ca024f
azizch29 <azizchadhar12345@gmail.com>
2024-03-05 17:53:23 +0500

    Add Crud Files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Navbar, Footer } from './pages/layout';
+import { Home } from './home';
+import { Products } from './products';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+function App() {
+  return(
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ Products' element={<Products/>}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+   
+    </>
+
+    
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +30,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/products.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Products() {
+    return(
+        <div className="container my-5">
+            <h2 className="text-center mb-3">Products Page</h2>
+        </div>
+    );
+}
